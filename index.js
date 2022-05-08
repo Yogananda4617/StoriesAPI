64e794a6b0e594935a8e77a736d345784ba6e2e2
Yogananda Potnuru <85452073+Yogananda4617@users.noreply.github.com>
2022-05-08 11:45:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 // API  in Node.js
-var express = require('express'); // requre the express framework
-var app = express();
-var fs = require('fs'); //require file system object
+const express = require('express'); // requre the express framework
+const app = express();
 const cors = require('cors');
-  
+
+const importData = require("./timestories.json");
+let port = process.env.PORT || 8000;  
+
   
 // CORS is enabled for all origins
 app.use(cors());
 
-
+app.get("/",(req,res) => {
+    res.send("redirect to /getStories to get data ");
+})
 // Endpoint to Get a list of users
 app.get('/getStories', function(req, res){
-    fs.readFile(__dirname + "/" + "timestories.json", 'utf8', function(err, data){
-        console.log(data);
-        res.end(data); 
-    });
+    res.send(importData);
 })
 
 // Create a server to listen at port 8080
-var server = app.listen(8000, function(){
-    var port = server.address().port
+app.listen(port, function(){
     console.log("TimeStories API is listening at http://localhost:", port)
 })
